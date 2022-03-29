d5249ac8044adf357b350ae2c0864f08c109aad4
YANA LIK <yana.v.lik@gmail.com>
2022-03-29 23:17:11 +0300

    module 3 activities

---
+++ SEF/src/sef/module3/activity/DateActivity.java
@@ -0,0 +1,10 @@
+package sef.module3.activity;
+
+import java.util.Date;
+
+public class DateActivity {
+    public static void main(String[] args) {
+        Date myDate = new Date();
+        System.out.println("Today is " + myDate);
+    }
+}
