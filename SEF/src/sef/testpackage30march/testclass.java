e63d44d0a77da779185d272e58f86d7d51044123
Doomer312 <poletelisnamize@gmail.com>
2022-03-30 19:00:57 +0300

    Commit of module3-9 activities, missing files

---
+++ SEF/src/sef/testpackage30march/testclass.java
@@ -0,0 +1,31 @@
+package sef.testpackage30march;
+
+
+public class testclass {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int total = 0;
+        // Example for completing code
+        for (int i = 1; i <= 50; i++) {
+            total = total + i;
+        }
+        System.out.println("Addition of Whole Numbers from 1 to 50 = " + total);
+
+        total = 0;
+
+        // Addition of Whole Numbers from 50 to 100
+        //TODO: complete the code. write for loop for i
+        int total2 = 0;
+        //TODO: complete the code. Write addition for variable.
+        for (int i = 50; i <= 100; i++) {
+            total2 = total2 + i;
+        }
+        System.out.println("Addition of Whole Numbers from 50 to 100 = "+total2);
+
+
+    }
+
+}
