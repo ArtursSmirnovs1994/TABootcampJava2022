500f8e7ec75ee469ce17289e593134b22efff5cc
YANA LIK <yana.v.lik@gmail.com>
2022-03-30 16:41:40 +0300

    module 9 activities

--- SEF/src/sef/module9/activity/Person.java
+++ SEF/src/sef/module9/activity/Person.java
@@ -7,11 +7,38 @@ public class Person {
 
     //TODO: Create getters and setters for weight and height
 
+    public int getWeight() {
+        return weight;
+    }
 
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
     //TODO: Create a method to calculate humans body mass index with two parameters for height and weight
-
+     public String getBMI() {
+     float result = weight/(height*height);
+     return String.valueOf(result);
+}
         //TODO: return String which indicates persons BMI. Calculation should be used weight/(height*height)
 
 
 
-}
+
+
+
+
+
+    }
+
+
+
+
+
--- SEF/src/sef/module9/activity/PersonTest.java
+++ SEF/src/sef/module9/activity/PersonTest.java
@@ -5,15 +5,46 @@ import junit.framework.TestCase;
 public class PersonTest extends TestCase {
 
     private Person person;
+    int testWeight = 60;
+    float testHeight = 172.5f;
 
     //TODO: create set up
 
     //TODO: create teardown
 
-    //TODO: create unit tests for getters and setters
+    protected void setUp() throws Exception {
+        super.setUp();
+        //	Initialize variables to be used here
+        person = new Person();
+        person.setHeight(testHeight);
+        person.setWeight(testWeight);
+    }
 
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+
+    //TODO: create unit tests for getters and setters
+    public void testGetterTest() {
+        assertEquals("GetterTest height", person.getHeight(),testHeight);
+        assertEquals(person.getWeight(),testWeight);
+    }
+
+    public void testSetterTest() {
+        int newWeight = 65;
+        person.setWeight(newWeight);
+        assertEquals(person.getWeight(),newWeight);
+        float newHeight = 170.6f;
+        person.setHeight(newHeight);
+        assertEquals(person.getHeight(),newHeight);
+
+    }
 
     //TODO: create unit test for getBodyMassIndex method
+        public void testgetBMI() {
+        assertEquals("0.0020163832", person.getBMI());
 
+}
 
 }
