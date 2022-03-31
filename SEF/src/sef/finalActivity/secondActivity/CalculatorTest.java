a3aaccf7b467e9068d54e2a2f7a7a175484bed69
YANA LIK <yana.v.lik@gmail.com>
2022-03-31 21:09:03 +0300

    Final activities second activity

--- SEF/src/sef/finalActivity/secondActivity/CalculatorTest.java
+++ SEF/src/sef/finalActivity/secondActivity/CalculatorTest.java
@@ -9,61 +9,71 @@ public class CalculatorTest extends TestCase {
 
     protected void setUp() throws Exception {
         super.setUp();
-        myCalculator = new Calculator();}
+        myCalculator = new Calculator();
+    }
 
-        protected void tearDown() throws Exception {
-            super.tearDown();
-        }
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 
-        public void testGetSum() {
+    public void testGetSum() {
         int x = 2;
-        int y = 4;
-        assertEquals(6, (myCalculator.getSum(x,y)));
+        int y = 3;
+        assertEquals(5, (myCalculator.getSum(x, y)));
 
         x = -1;
         y = -2;
 
-        assertEquals(-3, (myCalculator.getSum(x,y)));
+        assertEquals(-3, (myCalculator.getSum(x, y)));
 
-        }
-
-        public void testGetSubtraction() {
-            int x = 2;
-            int y = 4;
-            assertEquals(-2,(myCalculator.getSubtraction(x,y)));
-
-        }
-
-    public void testGetMultiplication () {
-        double x = 2;
-        double y = 4;
-        assertEquals(8.0,myCalculator.getMultiplication(x,y));
     }
 
-        public void testGetDivision() {
-            double x = 2;
-            double y = 4;
-            assertEquals(0.5,myCalculator.getDivision(x,y));
+    public void testGetSubtraction() {
+        int x = 2;
+        int y = 4;
+        assertEquals(-2, (myCalculator.getSubtraction(x, y)));
 
-        }
-public void testThrowException() {
+    }
+
+    public void testGetMultiplication() {
+        double x = 2;
+        double y = 4;
+        assertEquals(8.0, myCalculator.getMultiplication(x, y));
+    }
+
+    public void testGetDivision() {
+        double x = 2;
+        double y = 4;
+        assertEquals(0.5, myCalculator.getDivision(x, y));
+
+    }
+
+    public void testThrowException() {
         try {
-//            Integer x = null;
-//            Integer y = null;
-//            myCalculator.getSum(x,y);
-//            myCalculator.getSubtraction (x,y);
-            myCalculator.getMultiplication(0,1);
-            myCalculator.getDivision(1,0);
+            myCalculator.getMultiplication(0, 1);
+            myCalculator.getDivision(1, 0);
             new Calculator();
         } catch (Exception e) {
             fail();
 
+        } finally {
+            System.out.println("Try catch test executed");
         }
-        finally {
-            System.out.println("Try catch test executed");}
-        }
+    }
 
+    public void testThrowNullPointerException() {
+        try {
+            Integer x = null; int y = 2;
+            myCalculator.getSum(x,y);
+           myCalculator.getSubtraction(x,y);
+            new Calculator();
+        } catch (NullPointerException exception) {
+            int x = 3, y = 4;
+            System.out.println(x+y);
+            System.out.println(x-y);
         }
+    }
+}
 
 
 
