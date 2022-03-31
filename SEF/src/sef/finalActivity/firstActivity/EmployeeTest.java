7ba8368921f80422a50a9c59adbc54231f3b08e2
YANA LIK <yana.v.lik@gmail.com>
2022-03-31 22:02:46 +0300

    Final activities first activity

--- SEF/src/sef/finalActivity/firstActivity/EmployeeTest.java
+++ SEF/src/sef/finalActivity/firstActivity/EmployeeTest.java
@@ -41,6 +41,9 @@ public class EmployeeTest {
         emp4.setJobTitle("Developer");
         emp4.setSalary(75000);
         emp4.setCompany("Accenture");
+        System.out.println("My name is " + emp4.getName() + " and I am " + emp4.getAge() +
+                " years old\nI am working as " + emp4.getJobTitle() + " in " + emp4.getCompany() + " my salary is " + emp4.getSalary());
+
 
         Employee emp5 = new Employee();
         emp5.setName("Fred");
@@ -48,7 +51,10 @@ public class EmployeeTest {
         emp5.setJobTitle("Developer");
         emp5.setSalary(70000);
         emp5.setCompany("Accenture");
+        System.out.println("My name is " + emp5.getName() + " and I am " + emp5.getAge() +
+                " years old\nI am working as " + emp5.getJobTitle() + " in " + emp5.getCompany() + " my salary is " + emp5.getSalary());
 
+        System.out.println("----------------------------------------------------");
 
         Employee[] employee = new Employee[5];
 
@@ -59,16 +65,20 @@ public class EmployeeTest {
         Employee emp5s = new Employee(emp5.getJobTitle(),emp5.getCompany(),emp5.getSalary());
 
         employee[0]=emp1s;
+        employee[0].setName("Alex");
         employee[1]=emp2s;
+        employee[1].setName("Fiona");
         employee[2]=emp3s;
+        employee[2].setName("Andris");
         employee[3]=emp4s;
+        employee[3].setName("Berta");
         employee[4]=emp5s;
-
+        employee[4].setName("Fred");
         Arrays.sort(employee);
 
     int i=0;
     for(Employee temp: employee){
-    System.out.println("Employee " + ++i + " " + temp.getJobTitle() + " Salary: " + temp.getSalary());}
+    System.out.println("Employee " + ++i + " " + temp.getName() + " " + temp.getJobTitle() + " Salary: " + temp.getSalary());}
 
 
     }
