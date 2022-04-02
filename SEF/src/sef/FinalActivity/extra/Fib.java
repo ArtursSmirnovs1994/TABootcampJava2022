bea605ce16a03bf063263ebe0b53a97d6d7b1141
Ilja Carenko <icarenko@gmail.com>
2022-04-02 17:07:13 +0300

    Fibonacci with BigInteger is madness

--- SEF/src/sef/FinalActivity/extra/Fib.java
+++ SEF/src/sef/FinalActivity/extra/Fib.java
@@ -2,24 +2,27 @@ package sef.FinalActivity.extra;
 
 import javax.annotation.Nullable;
 import java.io.Console;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Fib {
-    public static Long calculateFibonacci(Long numberN) {
+    public static BigInteger calculateFibonacci(Long numberN) {
         return calculateFibonacci(numberN, null);
     }
 
     /* Calculates the Fibonacci sequence using dynamic programming paradigm
-    * Returns 0 for negative numbers*/
-    private static Long calculateFibonacci(Long numberN, @Nullable Map<Long, Long> storedResults) {
+     * Returns 0 for negative numbers*/
+    private static BigInteger calculateFibonacci(Long numberN, @Nullable Map<Long, BigInteger> storedResults) {
         if (numberN == 0) {
-            return 0L;
+            return BigInteger.ZERO;
         }
-        if (numberN < 0) {return 0L; }
+        if (numberN < 0) {
+            return BigInteger.ZERO;
+        }
         if (numberN <= 2) {
-            return 1L;
+            return BigInteger.ONE;
         }
         if (storedResults == null) {
             storedResults = new HashMap<>();
@@ -27,13 +30,17 @@ public class Fib {
         if (storedResults.containsKey(numberN)) {
             return storedResults.get(numberN);
         }
-        Long result = (calculateFibonacci(numberN - 1, storedResults) + calculateFibonacci(numberN - 2, storedResults));
+        BigInteger result = (calculateFibonacci(numberN - 1, storedResults).add(
+                calculateFibonacci(numberN - 2, storedResults))
+        );
         storedResults.put(numberN, result);
         return result;
     }
 
-    final Integer MAX_INPUT = 90;
+    final Integer MAX_INPUT = 5000; // HashMap may break at about 6600
+
     enum ValidationResult {USER_EXIT, MAX_EXCEEDED, NEGATIVE_NUMBER, NOT_A_NUMBER, EMPTY, OK}
+
     public void calculateByUserInput() {
         Scanner c = new Scanner(System.in);
 
@@ -43,15 +50,26 @@ public class Fib {
             String input = c.nextLine().trim();
             switch (validateInput(input)) {
                 case OK -> {
-                    Long result = calculateFibonacci( Long.valueOf(input) );
+                    BigInteger result = calculateFibonacci( Long.valueOf(input) );
                     System.out.printf("Calculated result is %d%n", result);
                 }
-                case USER_EXIT -> { isExit = true; System.out.println("Bye!"); }
-                case MAX_EXCEEDED -> {System.out.println("Maximum N exceeded");}
-                case NEGATIVE_NUMBER -> { System.out.println("Negative N is not allowed"); }
-                case EMPTY -> { }
-                case NOT_A_NUMBER -> { System.out.println("Input was not an integer"); }
-                default -> { }
+                case USER_EXIT -> {
+                    isExit = true;
+                    System.out.println("Bye!");
+                }
+                case MAX_EXCEEDED -> {
+                    System.out.println("Maximum N exceeded");
+                }
+                case NEGATIVE_NUMBER -> {
+                    System.out.println("Negative N is not allowed");
+                }
+                case EMPTY -> {
+                }
+                case NOT_A_NUMBER -> {
+                    System.out.println("Input was not an integer");
+                }
+                default -> {
+                }
             }
         } while (!isExit);
         c.close();
@@ -59,14 +77,23 @@ public class Fib {
 
 
     private ValidationResult validateInput(String input) {
-        if (input.length() == 0) {return ValidationResult.EMPTY;}
-        if (input.equals("Q") || input.equals("q")) { return ValidationResult.USER_EXIT; }
-        try {
-            Long.valueOf(input);
+        if (input.length() == 0) {
+            return ValidationResult.EMPTY;
         }
-        catch (NumberFormatException nfe) { return ValidationResult.NOT_A_NUMBER; }
-        if (Integer.parseInt(input) > MAX_INPUT) { return ValidationResult.MAX_EXCEEDED; }
-        if (Integer.parseInt(input) < 0) { return ValidationResult.NEGATIVE_NUMBER; }
+        if (input.equals("Q") || input.equals("q")) {
+            return ValidationResult.USER_EXIT;
+        }
+        try {
+            new BigInteger(input);
+        } catch (NumberFormatException nfe) {
+            return ValidationResult.NOT_A_NUMBER;
+        }
+        if (Integer.parseInt(input) > MAX_INPUT) {
+            return ValidationResult.MAX_EXCEEDED;
+        }
+        if (Integer.parseInt(input) < 0) {
+            return ValidationResult.NEGATIVE_NUMBER;
+        }
         return ValidationResult.OK;
     }
 
--- SEF/src/sef/FinalActivity/extra/FibTest.java
+++ SEF/src/sef/FinalActivity/extra/FibTest.java
@@ -3,18 +3,22 @@ package sef.FinalActivity.extra;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigInteger;
+
 import static sef.FinalActivity.extra.Fib.calculateFibonacci;
 
 public class FibTest {
     @Test
     public void testFibonacci() {
-        Assert.assertEquals((Long)0L, calculateFibonacci(0L) );
-        Assert.assertEquals((Long)5L, calculateFibonacci(5L) );
-        Assert.assertEquals((Long)8L, calculateFibonacci(6L) );
-        Assert.assertEquals((Long)13L, calculateFibonacci(7L) );
-        Assert.assertEquals((Long)12586269025L, calculateFibonacci(50L) );
-        Assert.assertEquals((Long)2880067194370816120L, calculateFibonacci(90L) );
-        Assert.assertEquals((Long)0L, calculateFibonacci(-9L) );
+        Assert.assertEquals(BigInteger.ZERO, calculateFibonacci(0L) );
+        Assert.assertEquals(new BigInteger(String.valueOf(5)), calculateFibonacci(5L) );
+        Assert.assertEquals(new BigInteger(String.valueOf(8)), calculateFibonacci(6L) );
+        Assert.assertEquals(new BigInteger(String.valueOf(13)), calculateFibonacci(7L) );
+        Assert.assertEquals(new BigInteger(String.valueOf(12586269025L)), calculateFibonacci(50L) );
+        Assert.assertEquals(new BigInteger(String.valueOf(2880067194370816120L)), calculateFibonacci(90L) );
+        Assert.assertEquals(new BigInteger(
+                "43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875"
+        ), calculateFibonacci(1000L) );
+        Assert.assertEquals(BigInteger.ZERO, calculateFibonacci(-9L) );
     }
-
 }
